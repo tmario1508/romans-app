13c1ce1224b5bd2a24b0986175ceea7d7abac08f
Jose Ignacio Vaqueiro Gonzalez <ignaciovaqdevelop@hotmail.com>
2020-11-16 21:16:46 -0600

    Primeras Paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/navigation/topbar/topbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ConocenosComponent } from './pages/conocenos/conocenos.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { PerfilComponent } from './pages/usuario/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    HomeComponent,
+    MenuComponent,
+    LoginComponent,
+    ConocenosComponent,
+    CarritoComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
